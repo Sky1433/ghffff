b68bdc5e425294632eed01d8375bf2cec3f9622d
Sky1433 <samaanawaf@gmail.com>
2022-12-15 10:12:53 +0300

    Initial Commit

---
+++ ghffff/ghffffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghffffApp.swift
+//  ghffff
+//
+//  Created by marah aljuhani on 21/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ghffffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
